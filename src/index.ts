004f615c7c3fd17afeda93e07b92d366643139ab
rafalzawadzki <iam@rafalzawadzki.com>
2024-07-06 15:41:23 +0200

    - export FunctionSchema type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { generateOperationName } from './generate-operation-name';
 export { convertRawOpenAPISpecToOpenAIFunctions } from './openapi-to-functions';
+export { FunctionSchema } from './types';
